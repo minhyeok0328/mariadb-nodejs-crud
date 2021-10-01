a7330cbecaa2468f50a07ecfb5927c67a8523649
Min-Hyeok <alsgur0008@naver.com>
2021-10-01 18:43:50 +0900

    도커 컨테이너 네트워크 설정 추가,
    nodejs 시작 시 403 뜨는거 수정해야 함

--- src/database/index.js
+++ src/database/index.js
@@ -1,7 +1,7 @@
 import mysql from 'mysql2/promise';
 
 const connection = await mysql.createConnection({
-    host: 'localhost',
+    host: 'db',
     user: 'root',
     password: '1234',
     database: 'crud'
