c1451dcdf9d914fca341afe5b8eac68720f2e10e
Min-Hyeok <alsgur0008@naver.com>
2021-10-14 18:11:50 +0900

    graphql 패키지 추가

--- index.js
+++ index.js
@@ -3,6 +3,20 @@ import cors from 'cors';
 import ApiV1Router from './src/router/apiV1Router/index.js';
 import cookieParser from 'cookie-parser';
 import { authCheck } from './src/middleware/authCheck.js';
+import { graphqlHTTP } from 'express-graphql';
+import { buildSchema } from 'graphql';
+
+const schema = buildSchema(`
+    type Query {
+        hello: String
+    }
+`);
+
+const rootValue = {
+    hello() {
+        return 'Hello World!!';
+    }
+}
 
 const app = express();
 
@@ -11,6 +25,12 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(authCheck);
 app.use('/api/v1', ApiV1Router);
+app.use('/graphql', graphqlHTTP({
+    schema,
+    rootValue,
+    graphiql: true
+}));
+
 app.listen(8080, () => {
     console.log(`server running! / node -v: ${process.version}`);
 });
