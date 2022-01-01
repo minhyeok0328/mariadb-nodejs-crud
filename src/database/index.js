e880964402d5bc91c4fc121e4d9823629a187bfd
Minhyeok <alsgur0008@naver.com>
2022-01-02 02:21:25 +0900

    heroku db 환경변수 수정

--- src/database/index.js
+++ src/database/index.js
@@ -1,10 +1,10 @@
 import mysql from 'mysql2/promise';
 
 const connection = await mysql.createConnection({
-    host: process.env.NODE_ENV == 'production' ? process.env.JAWSDB_MARIA_URL : 'db',
-    user: process.env.NODE_ENV == 'production' ? process.env.JAWSDB_MARIA_USER : 'root',
-    password: process.env.NODE_ENV == 'production' ? process.env.JAWSDB_MARIA_PASSWORD : '1234',
-    database: process.env.NODE_ENV == 'production' ? process.env.JAWSDB_MARIA_DATABASE : 'crud'
+    host: process.env.NODE_ENV == 'production' ? process.env.HOST : 'db',
+    user: process.env.NODE_ENV == 'production' ? process.env.USERNAME : 'root',
+    password: process.env.NODE_ENV == 'production' ? process.env.PASSWORD : '1234',
+    database: process.env.NODE_ENV == 'production' ? process.env.DATABASE : 'crud'
 });
 
 export default connection;
