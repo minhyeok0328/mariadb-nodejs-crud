a44039f7f32064fa6a1adaf558554a31fb1b9741
Min-Hyeok <alsgur0008@naver.com>
2022-02-15 18:07:23 +0900

    docker 설치 파일 추가

---
+++ install-docker.sh
@@ -0,0 +1,12 @@
+sudo apt update
+sudo apt-get install curl -y
+sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+sudo apt update
+sudo apt-cache policy docker-ce
+sudo apt install docker-ce -y
+systemctl status docker
+curl -L https://github.com/docker/compose/releases/download/v2.2.3/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+docker-compose --version
